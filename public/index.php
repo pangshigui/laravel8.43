6aca487357cbba111c57b1b42157fe9c6f7eab21
pangshigui <pangshigui@163.com>
2021-05-27 20:13:17 +0800

    delete test reset

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-//test reset hard
+
 /*
 |--------------------------------------------------------------------------
 | Check If Application Is Under Maintenance
